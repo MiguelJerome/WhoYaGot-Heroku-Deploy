7700c107306e03b7ef823cb34216f1c9e40f092a
MiguelJerome <sharkmaycry@gmail.com>
2022-11-04 04:32:34 -0400

    heroku testing

--- server/server.js
+++ server/server.js
@@ -21,11 +21,8 @@ const server = new ApolloServer({
   context: authMiddleware
 });
 
-
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
 
 // Serve up static assets
 if (process.env.NODE_ENV === 'production') {
@@ -36,21 +33,11 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-
-
-
-
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
 await server.start();
 // integrate our Apollo server with the Express application as middleware
 server.applyMiddleware({ app });
-
-
-
-
-
-
 
 db.once('open', () => {
     app.listen(PORT, () => {
